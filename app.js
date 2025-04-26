259a566896f71675307a4a986c0bddeab7165523
AbdurRahmanSudais01 <abdurrahmansudais2149@gmail.com>
2025-04-26 09:57:18 +0500

    new

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function generateRandomID(length = 10) {
+function generateRandomID(length = 6) {
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
     for (let i = 0; i < length; i++) {
@@ -13,38 +13,52 @@ function setRandomID() {
     userIdField.value = generateRandomID();
 }
 setRandomID();
-
-document.getElementById("form").addEventListener('submit', function(e)
-{
+document.getElementById("form").addEventListener('submit', function(e) {
     e.preventDefault();
     
     setRandomID();
-    let userId=document.getElementById("userId").value;
-    let userFirstName=document.getElementById("firstName").value;
-    let userLastName=document.getElementById("lastName").value;
-    let userAge=document.getElementById("age").value;
-    let userGender=document.getElementById("gender").value;
-    let userEmail=document.getElementById("email").value;
-    let userContact=document.getElementById("contact").value;
-    let userAddress=document.getElementById("address").value;
-    
-    let newdiv = document.createElement("div");
-    document.childNodes[1].childNodes[2].appendChild(newdiv),
-    newdiv.setAttribute("id", "formSubmitted");
-    let outputDiv = document.getElementById("formSubmitted");
-    outputDiv.innerHTML = `
+    let userId = document.getElementById("userId").value;
+    let userFirstName = document.getElementById("firstName").value;
+    let userLastName = document.getElementById("lastName").value;
+    let userAge = document.getElementById("age").value;
+    let userGender = document.getElementById("gender").value;
+    let userEmail = document.getElementById("email").value;
+    let userContact = document.getElementById("contact").value;
+    let userAddress = document.getElementById("address").value;
+
+    // Create a new div for each submission
+    let newDiv = document.createElement("div");
+    newDiv.className = "formSubmitted";
+
+    // Create a new user object
+    let user = new User(userId, userFirstName, userLastName, userAge, userGender, userEmail, userContact, userAddress);
+
+    newDiv.innerHTML = `
     <h1>Form Submitted</h1>
-    <p><strong>Id:</strong> ${userId}</p>
-    <p><strong>First Name:</strong> ${userFirstName}</p>
-    <p><strong>Last Name:</strong> ${userLastName}</p>
-    <p><strong>Age:</strong> ${userAge}</p>
-    <p><strong>Gender:</strong> ${userGender}</p>
-    <p><strong>Email:</strong> ${userEmail}</p>
-    <p><strong>contact:</strong> ${userContact}</p>
-    <p><strong>Address:</strong> ${userAddress}</p>
-    `
-})
+    <p><strong>Id:</strong> ${user.id}</p>
+    <p><strong>First Name:</strong> ${user.firstName}</p>
+    <p><strong>Last Name:</strong> ${user.lastName}</p>
+    <p><strong>Age:</strong> ${user.age}</p>
+    <p><strong>Gender:</strong> ${user.gender}</p>
+    <p><strong>Email:</strong> ${user.email}</p>
+    <p><strong>Contact:</strong> ${user.contact}</p>
+    <p><strong>Address:</strong> ${user.address}</p>`;
 
+    document.body.appendChild(newDiv);
+    usersList.push(user);
+    console.log(usersList);
+    
+});
 
-
-
+function User(id, firstName, lastName, age, gender, email, contact, address) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender; 
+    this.email = email;
+    this.contact = contact; 
+    this.address = address;
+ 
+};   
+ let usersList = [];
