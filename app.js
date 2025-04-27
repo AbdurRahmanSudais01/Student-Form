f3ed44ae4a3e1a57b020e3d9b06d294271b68758
AbdurRahmanSudais01 <abdurrahmansudais2149@gmail.com>
2025-04-27 23:44:48 +0500

    some changes

--- app.js
+++ app.js
@@ -12,28 +12,30 @@ function setRandomID() {
     const userIdField = document.getElementById('userId');
     userIdField.value = generateRandomID();
 }
-setRandomID();
+
+setRandomID(); // Set a new random ID
 document.getElementById("form").addEventListener('submit', function(e) {
     e.preventDefault();
+    let usersList = [];
     
-    setRandomID();
-    let userId = document.getElementById("userId").value;
-    let userFirstName = document.getElementById("firstName").value;
-    let userLastName = document.getElementById("lastName").value;
-    let userAge = document.getElementById("age").value;
-    let userGender = document.getElementById("gender").value;
-    let userEmail = document.getElementById("email").value;
-    let userContact = document.getElementById("contact").value;
-    let userAddress = document.getElementById("address").value;
-
+    let userId = e.target.userId.value;
+    let userFirstName = e.target.firstName.value;
+    let userLastName = e.target.lastName.value;
+    let userAge = e.target.age.value;
+    let userGender = e.target.gender.value;
+    let userEmail = e.target.email.value;
+    let userContact = e.target.contact.value;
+    let userAddress = e.target.address.value;
+    
     // Create a new div for each submission
     let newDiv = document.createElement("div");
     newDiv.className = "formSubmitted";
-
+    
     // Create a new user object
     let user = new User(userId, userFirstName, userLastName, userAge, userGender, userEmail, userContact, userAddress);
-
+    
     newDiv.innerHTML = `
+    <button id="deleteData" onclick="this.parentElement.remove()">Delete</button>
     <h1>Form Submitted</h1>
     <p><strong>Id:</strong> ${user.id}</p>
     <p><strong>First Name:</strong> ${user.firstName}</p>
@@ -43,13 +45,20 @@ document.getElementById("form").addEventListener('submit', function(e) {
     <p><strong>Email:</strong> ${user.email}</p>
     <p><strong>Contact:</strong> ${user.contact}</p>
     <p><strong>Address:</strong> ${user.address}</p>`;
-
+    
     document.body.appendChild(newDiv);
     usersList.push(user);
-    console.log(usersList);
     
+    e.target.firstName.value = '';
+    e.target.lastName.value = '';
+    e.target.age.value = '';
+    e.target.gender.value = '';
+    e.target.email.value = '';
+    e.target.contact.value = '';
+    e.target.address.value = '';
+    
+    setRandomID(); // Set a new random ID on form submission
 });
-
 function User(id, firstName, lastName, age, gender, email, contact, address) {
     this.id = id;
     this.firstName = firstName;
@@ -61,4 +70,3 @@ function User(id, firstName, lastName, age, gender, email, contact, address) {
     this.address = address;
  
 };   
- let usersList = [];
